96433ccd7e60d238e8995b1d0a027ffcc6a16ea6
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-06-20 00:35:07 +0200

    Add leaflet map

--- app/imports/ui/Story.jsx
+++ app/imports/ui/Story.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import { StoryTitle } from './StoryTitle.jsx';
 import { StoryChapter } from './StoryChapter.jsx';
@@ -19,13 +21,27 @@ export class Story extends React.Component {
 
   render() {
     const TheTitle = this.state.title;
+    const position = [53, 8]
     return (
       <div>
         <TheTitle />
         <button onClick={this.handleAddChapter}>Add Chapter</button>
+
         {this.state.chapters.map((Chapter,i) => {
           return <Chapter key={i.toString()} />
         })}
+
+        <Map center={position} zoom={13}>
+          <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
