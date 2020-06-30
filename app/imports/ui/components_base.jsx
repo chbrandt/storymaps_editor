1a1d295ab819cccbaea615d2ad6bda5a3c08f56a
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-06-30 19:14:51 +0200

    Add JSDoc/ESLint packages and a first example on "base" components

--- app/imports/ui/components_base.jsx
+++ app/imports/ui/components_base.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 
 import { capitalize } from '../api/utils.js';
 
-export const Select = (props) => {
-  /*
-  Properties:
-  - onChange :
-  - items: list of values for selection dropdown/menu list
-  - placeholder: dummy item in the selection list (empty/"" value associated)
-  - label : (internal) "name" for (HTML) <select> element (default is 'selector')
-  */
+/**
+ *
+ * @callback onChange Some desc
+ * @param {*} value Value returned to pattern component
+ */
+
+/**
+ * Render a select element with `items`
+ *
+ * @param {object} props Props of our component
+ * @param {string[]} props.items Items to offer as Options
+ * @param {onChange} props.onChange Callback for when new item is selected
+ * @param {string} [props.label="selector"] Used as internal 'select' element name
+ * @param {string} [props.placeholder="Options"] Expression to indicate content (a tip)
+ * @param {number} props.selected Selected option
+ * @returns {object} Select element
+ */
+const Select = (props) => {
   const handleChange = (e) => {
     if(e.target.value){
       props.onChange(e.target.value)
@@ -37,12 +47,17 @@ export const Select = (props) => {
   );
 }
 
-export const InputSelect = (props) => {
-  /*
-  Properties:
-  - label : (optional) label to put next to the input field
-  - placeholder : (optional) placeholder for the input field
-  */
+
+/**
+ * Render an `input` element with options to select
+ *
+ * @param {object} props Props of our component
+ * @param {onChange} props.onChange Callback to handle input 'e.target.value'
+ * @param {string} [props.label] Used as internal 'select' element name
+ * @param {string} [props.placeholder] Expression to indicate content (a tip)
+ * @returns {string} Input value
+ */
+const InputSelect = (props) => {
   const handleChange = (e) => {
     props.onChange(e.target.value);
   }
@@ -65,17 +80,28 @@ export const InputSelect = (props) => {
 }
 
 
-export const InputNumber = (props) => {
+/**
+ * Render an `input` element for numerical values
+ *
+ * @param {object} props Props of our component
+ * @param {onChange} props.onChange Callback to handle input 'e.target.value'
+ * @param {number} [props.value] Used as `input` (default) value
+ * @param {number} [props.min] Used as input `min` value boundary
+ * @param {number} [props.max] Used as input `max` value boundary
+ * @param {string} [props.label] Used as input label
+ * @param {string|number} [props.placeholder] Expression to indicate content (a tip)
+ * @returns {string} Input value
+ */
+const InputNumber = (props) => {
 // TODO: make this a class component to keep a state "number" to be validated.
   const handleChange = (e) => {
     props.onChange(Number(e.target.value));
   };
-  const placeholder = props.placeholder || "";
   return (
     <label>
       {props.label}
       <input type="number"
-              placeholder={placeholder}
+              placeholder={props.placeholder}
               defaultValue={props.value}
               min={props.min}
               max={props.max}
@@ -87,13 +113,24 @@ export const InputNumber = (props) => {
 }
 
 
-export const InputText = (props) => {
-  const placeholder = props.placeholder || "";
+/**
+ * Render an `input` element with "spell-check" enabled
+ *
+ * @param {object} props Props of our component
+ * @param {onChange} props.onChange Callback to handle input 'e.target.value'
+ * @param {number} [props.value] Used as `input` (default) value
+ * @param {number} [props.min] Used as input `min` value boundary
+ * @param {number} [props.max] Used as input `max` value boundary
+ * @param {string} [props.label] Used as input label
+ * @param {string} [props.placeholder] String for bla
+ * @returns {string} Input value
+ */
+const InputText = (props) => {
   return (
     <label>
       {props.label}
       <input type="text" spellCheck="true"
-        placeholder={placeholder}
+        placeholder={props.placeholder}
         defaultValue={props.value}
         onKeyDown={(e) => {if(e.keyCode == 13){props.onChange(e.target.value)}}}
         onBlur={(e) => {props.onChange(e.target.value)}}
@@ -103,15 +140,30 @@ export const InputText = (props) => {
 }
 
 
-export const TextArea = (props) => {
-  const min = props.min || 10;
+/**
+ * Render a `textarea` element
+ *
+ * @param {object} props Props of our component
+ * @param {onChange} props.onChange Callback to handle input 'e.target.value'
+ * @param {number} [props.value] Used as `input` (default) value
+ * @param {number} [props.min=0] Used as input `min` value boundary
+ * @param {number} [props.max=500] Used as input `max` value boundary
+ * @param {number} [props.rows=5] Used as input `min` value boundary
+ * @param {number} [props.cols=40] Used as input `max` value boundary
+ * @param {string} [props.label] Used as input label
+ * @returns {string} Input value
+ */
+const TextArea = (props) => {
+  const min = props.min || 0;
   const max = props.max || 500;
   const placeholder = `text (min:${min},max:${max})..`;
+  const rows = props.rows || 5;
+  const cols = props.cols || 40;
   return (
     <label>
       {props.label}
       <br/>
-        <textarea rows={5} cols={40} wrap="soft" spellCheck="true"
+        <textarea rows={rows} cols={cols} wrap="soft" spellCheck="true"
           placeholder={placeholder}
           minLength={min} maxLength={max}
           defaultValue={props.value}
@@ -121,3 +173,5 @@ export const TextArea = (props) => {
     </label>
   );
 }
+
+export {Select, InputSelect, InputNumber, InputText, TextArea};
